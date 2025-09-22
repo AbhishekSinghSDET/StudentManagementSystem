fdbe4b6b810ea2fdefd4f3a5def59116e87184ae
abhi2560 <abhisingh2560@gmail.com>
2025-09-22 16:24:12 +0530

    Comparator and Sorting

--- src/com/studentapp/Main2.java
+++ src/com/studentapp/Main2.java
@@ -1,6 +1,8 @@
 package com.studentapp;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main2 {
@@ -23,25 +25,56 @@ public class Main2 {
         s2.enrollCourse("DevOps");
 
 
-        Student s3 = new Student("Rohan",23,"S-3");
+        Student s3 = new Student("Kartik",23,"S-3");
         s3.enrollCourse("DevOps");
+
+        Student s4 = new Student("Bharat",23,"S-4");
+        s3.enrollCourse("DSA");
 
         studentList.add(s1);
         studentList.add(s2);
         studentList.add(s3);
+        studentList.add(s4);
 
         Student result = findStudentById("S-1");
         System.out.println("Result " +result);
 
+        Student result1 = findStudentById("S-10");
+        System.out.println("Result " +result1);
 
+        sortByName();
+    }
+
+
+
+    //Function created to sort object i.e student name in ascending order
+    private static void sortByName() {
+//        Comparator<Student> studentNameComparator = new Comparator<Student>() {
+//            @Override
+//            public int compare(Student o1, Student o2) {
+//                return o1.getName().compareTo(o2.getName());  // comparison happen lexographically
+//            }
+//        };
+
+        //Optimized one using Lamda Expression
+        Comparator<Student> studentNameComparator = (o1,o2) -> o1.getName().compareTo(o2.getName());
+
+        Collections.sort(studentList,studentNameComparator);
+        System.out.println(studentList);
     }
 
 
     //Created this function to find if specific student Id is present
     public static Student findStudentById(String studentId){
-        Student result = studentList.stream().filter(x -> x.getStudentId().equalsIgnoreCase(studentId))
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("No data found!!"));
+        Student result = null;
+        try {
+            result = studentList.stream().filter(x -> x.getStudentId().equalsIgnoreCase(studentId))
+                    .findFirst()
+                    .orElseThrow(() -> new RuntimeException("No data found!!"));
+        }
+        catch (RuntimeException e){
+            System.err.println("Student with ID "+studentId+" not found");
+        }
         return result;
     }
 }
